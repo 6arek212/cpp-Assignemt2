b003553e23cfc43ab177bb4e4510cf5ab319f26e
tarik <tarik.id.10@hotmail.com>
2022-03-23 22:59:02 +0200

    update datastructure used

--- Notebook.cpp
+++ Notebook.cpp
@@ -5,71 +5,71 @@
 using namespace ariel;
 using namespace std;
 
-void Notebook::addLines(int page, int rows, Direction dir, int length)
+void validateInput(int page, int row, int column, Direction dir, int length = 0, const string &data = "")
 {
-    int lg = (dir == Direction::Horizontal) ? rows : rows + length;
-    for (int i = 0; i <= lg; i++)
-    {
-        if (this->_pages[page].count(i) == 0)
-        {
-            this->_pages[page][i] = new char[_COLUMN_LENGTH];
-            for (int k = 0; k < _COLUMN_LENGTH; k++)
-            {
-                this->_pages[page][i][k] = '_';
-            }
-        }
-    }
-}
-
-bool isValidString(string str)
-{
-    for (size_t i = 0; i < str.size(); i++)
-    {
-        if (str[i] == '~')
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-void Notebook::write(int page, int row, int column, Direction dir, const string &data)
-{
-    if ((int)page < 0 || (int)row < 0 || (int)column < 0)
+    if (page < 0 || row < 0 || column < 0 || length < 0)
     {
         throw invalid_argument("Error ,row , page , column  -> neither can be a negative value !");
     }
 
-    if (!isValidString(data))
+    if (length == 0)
     {
-        throw invalid_argument("the input string cant have '~' or '_'");
+        if (dir == Direction::Horizontal && (column + (int)data.length() - 1 >= _COLUMN_LENGTH))
+        {
+            throw invalid_argument("Error ,column + data length must be less than COLUMN LENGTH");
+        }
+        if (data.find('~') != string::npos)
+        {
+            throw invalid_argument("the input string cant have '~'");
+        }
     }
-
-    if (dir != Direction::Vertical && (column + (int)data.length() - 1 >= _COLUMN_LENGTH))
+    else
     {
-        throw invalid_argument("Error ,column + data length must be less than COLUMN LENGTH");
+        if (dir == Direction::Horizontal && (column + length - 1 >= _COLUMN_LENGTH))
+        {
+            throw invalid_argument("Error ,column + data length must be less than COLUMN LENGTH");
+        }
     }
+}
 
-    addLines(page, row, dir, data.length());
+void Notebook::updatePageIndexs(int page, int row, Direction dir, int length)
+{
+    if (_pages[page].first_written_row == -1)
+    {
+        _pages[page].first_written_row = (row > 0) ? row - 1 : row;
+        _pages[page].last_written_row = (dir == Direction::Vertical) ? row + length : row;
+    }
+    else if (row <= _pages[page].first_written_row)
+    {
+        _pages[page].first_written_row = (row > 0) ? row - 1 : row;
+        _pages[page].last_written_row = (dir == Direction::Vertical && row + length > _pages[page].last_written_row) ? row + length : row;
+    }
+}
 
+void Notebook::write(int page, int row, int column, Direction dir, const string &data)
+{
+    validateInput(page, row, column, dir, 0, data);
+    // check intersection
     for (int i = 0; i < data.size(); i++)
     {
-        if ((dir == Direction::Horizontal && this->_pages[page][row][column + i] != '_') || (dir == Direction::Vertical && this->_pages[page][row + i][column] != '_'))
+        if ((dir == Direction::Horizontal && this->_pages[page].page[row][column + i].ch != '_') || (dir == Direction::Vertical && this->_pages[page].page[row + i][column].ch != '_'))
         {
             throw invalid_argument("Error , writing one of the letter intersect with another");
         }
     }
+
+    updatePageIndexs(page, row, dir, (int)data.size());
 
     int i = 0;
     for (char const &ch : data)
     {
         if (dir == Direction::Horizontal)
         {
-            this->_pages[page][row][column + i] = ch;
+            this->_pages[page].page[row][column + i].ch = ch;
         }
         else
         {
-            this->_pages[page][row + i][column] = ch;
+            this->_pages[page].page[row + i][column].ch = ch;
         }
         i++;
     }
@@ -77,28 +77,17 @@ void Notebook::write(int page, int row, int column, Direction dir, const string 
 
 string Notebook::read(int page, int row, int column, Direction dir, int length)
 {
-    if ((int)page < 0 || (int)row < 0 || (int)column < 0 || (int)length < 0)
-    {
-        throw invalid_argument("Error ,row , page , column , length -> neither can be a negative value !");
-    }
-
-    if (dir != Direction::Vertical && (column + length - 1 >= _COLUMN_LENGTH))
-    {
-        throw invalid_argument("Error ,column + data length must be less than COLUMN LENGTH");
-    }
-
-    addLines(page, row, dir, length);
-
+    validateInput(page, row, column, dir, length);
     string str;
     for (int i = 0; i < length; i++)
     {
         if (dir == Direction::Horizontal)
         {
-            str += this->_pages[page][row][column + i];
+            str += this->_pages[page].page[row][column + i].ch;
         }
         else
         {
-            str += this->_pages[page][row + i][column];
+            str += this->_pages[page].page[row + i][column].ch;
         }
     }
     return str;
@@ -106,34 +95,18 @@ string Notebook::read(int page, int row, int column, Direction dir, int length)
 
 void Notebook::erase(int page, int row, int column, Direction dir, int length)
 {
-    if ((int)page < 0 || (int)row < 0 || (int)column < 0 || (int)length < 0)
-    {
-        throw invalid_argument("Error ,row , page , column , length -> neither can be a negative value !");
-    }
-
-    if (dir != Direction::Vertical && (column + length - 1 >= _COLUMN_LENGTH))
-    {
-        throw invalid_argument("Error ,column + data length must be less than COLUMN LENGTH");
-    }
-
-    addLines(page, row, dir, length);
-    // for (int i = 0; i < length; i++)
-    // {
-    //     if ((dir == Direction::Horizontal && this->_pages[page][row][column + i] == '~') || (dir == Direction::Vertical && this->_pages[page][row + i][column] == '~'))
-    //     {
-    //         throw invalid_argument("Error , Already removed this char");
-    //     }
-    // }
+    validateInput(page, row, column, dir, length);
+    updatePageIndexs(page, row, dir, length);
 
     for (int i = 0; i < length; i++)
     {
         if (dir == Direction::Horizontal)
         {
-            this->_pages[page][row][column + i] = '~';
+            this->_pages[page].page[row][column + i].ch = '~';
         }
         else
         {
-            this->_pages[page][row + i][column] = '~';
+            this->_pages[page].page[row + i][column].ch = '~';
         }
     }
 }
@@ -154,23 +127,27 @@ int countDigits(int x)
     return cnt;
 }
 
-int Notebook::findFirstWirttenLine(int page)
+void Notebook::printPage(int page, bool flag, int start_row, int end_row)
 {
-    for (int i = 0; _pages[page].count(i) != 0; i++)
+    for (int i = start_row; i <= end_row; i++)
     {
+        cout << i << "-";
+
+        int maxRowNumber = flag ? 3 : countDigits(_pages[page].last_written_row) + 1;
+
+        for (int k = 0; k < maxRowNumber - countDigits(i); k++)
+        {
+            cout << " ";
+        }
+
         for (int k = 0; k < _COLUMN_LENGTH; k++)
         {
-            if (_pages[page][i][k] != '_')
-            {
-                if (i > 0)
-                {
-                    return i - 1;
-                }
-                return i;
-            }
+            cout << _pages[page].page[i][k].ch << "";
         }
+        cout << endl;
     }
-    return 0;
+
+    cout << endl;
 }
 
 void Notebook::show(int page)
@@ -180,75 +157,17 @@ void Notebook::show(int page)
         throw invalid_argument("Error ,page cant be a negative value !");
     }
 
-    cout << endl
-         << "-----"
-         << "Notebook title: " << this->_title << "-----" << endl;
-
     cout
         << endl
         << "Page number " << page << endl
         << endl;
 
-    int starterLine = findFirstWirttenLine(page);
-
-    for (int i = starterLine; (_pages[page].count(i) != 0); i++)
+    if (_pages[page].first_written_row == -1)
     {
-        cout << i << "-";
-
-        for (int k = 0; k < countDigits(_pages[page].size()) + 1 - countDigits(i); k++)
-        {
-            cout << " ";
-        }
-
-        for (int k = 0; k < _COLUMN_LENGTH; k++)
-        {
-            cout << _pages[page][i][k] << "";
-        }
-        cout << endl;
+        printPage(page, true);
     }
-
-    cout << endl;
-}
-
-/**
- * @brief  This is the Show format
- *
- */
-
-/**
-    Page number 100
-
-
-    83-  ____________________________________________________________________________________________________
-    84-  ____________________________________________________________________________________________________
-    85-  ____________________________________________________________________________________________________
-    86-  ____________________________________________________________________________________________________
-    87-  ____________________________________________________________________________________________________
-    88-  ____________________________________________________________________________________________________
-    89-  ____________________________________________________________________________________________________
-    90-  ____________________________________________________________________________________________________
-    91-  ____________________________________________________________________________________________________
-    92-  ____________________________________________________________________________________________________
-    93-  ____________________________________________________________________________________________________
-    94-  ____________________________________________________________________________________________________
-    95-  ____________________________________________________________________________________________________
-    96-  ____________________________________________________________________________________________________
-    97-  ____________________________________________________________________________________________________
-    98-  ____________________________________________________________________________________________________
-    99-  ___________________________________________________~________________________________________________
-    100- __________________________________________________a~cd______________________________________________
-    101- ___________________________________________________~________________________________________________
-    102- ____________________________________________________________________________________________________
-
-**/
-
-Notebook::~Notebook()
-{
-    for (auto &pg : _pages)
+    else
     {
-        for (auto &ln : pg.second)
-        {
-            delete[] ln.second;
-        }
+        printPage(page, false, _pages[page].first_written_row, _pages[page].last_written_row);
     }
 }
--- Notebook.hpp
+++ Notebook.hpp
@@ -7,23 +7,34 @@ namespace ariel
 {
     const int _COLUMN_LENGTH = 100;
 
+    struct placeholder
+    {
+        char ch = '_';
+    };
+
+    struct page
+    {
+        map<int, placeholder[_COLUMN_LENGTH]> page;
+        int first_written_row = -1;
+        int last_written_row = -1;
+    };
+
     class Notebook
     {
 
     private:
         string _title;
-        map<int, map<int, char *>> _pages;
+        map<int, page> _pages;
 
     private:
-        void addLines(int page, int rows, Direction dir, int length = 1);
-        int findFirstWirttenLine(int page);
+        void updatePageIndexs(int page, int row, Direction dir, int length = 0);
+        void printPage(int page, bool flag, int start_row = 0, int end_row = 100);
 
     public:
         void write(int page, int row, int column, Direction dir, const string &data);
         string read(int page, int row, int column, Direction dir, int length);
         void erase(int page, int row, int column, Direction dir, int length);
         void show(int page);
-        ~Notebook();
     };
 
 }
